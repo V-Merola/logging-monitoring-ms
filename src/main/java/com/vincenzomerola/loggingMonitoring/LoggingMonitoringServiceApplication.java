a1ef14052f1af41cb2a79a5dc76340da78732a8b
V-Merola <vincenzo.merola18@gmail.com>
2024-08-26 19:10:34 +0200

    Initial Commit

---
+++ src/main/java/com/vincenzomerola/loggingMonitoring/LoggingMonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vincenzomerola.loggingMonitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingMonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingMonitoringServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vincenzomerola/loggingMonitoring/LoggingMonitoringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vincenzomerola.loggingMonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoggingMonitoringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
